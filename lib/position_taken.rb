2fb1d79543ff3c71bcdbb0158ae2d76b489263af
Jacob Levitin <jlevitin92@gmail.com>
2020-09-07 22:11:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,6 @@
 # code your #position_taken? method here!
-include binding.pry
 
-def position_taken?(board, index)
-  binding.pry
-  
+def position_taken?(board, index)  
   if board[index] == "X" or "O"
     true
     if board[index] == " " or ""
