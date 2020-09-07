28492cf803462dd09edb0904472fff71e5960544
Jacob Levitin <jlevitin92@gmail.com>
2020-09-07 21:53:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || "" 
+  if board[index] == " " || "" not "X" or "O"
     false
   elsif board[index] == "X" || "O"
     true
